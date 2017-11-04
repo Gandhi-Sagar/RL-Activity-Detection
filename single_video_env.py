d57750010cb7b507af5034d5e87b26695fd74395
Gandhi-Sagar <Gandhi.Sagar.GS@gmail.com>
2017-11-04 15:59:24 -0400

    completed code, seems fishy atm
    structurally works, functionality is doubtful

--- single_video_env.py
+++ single_video_env.py
@@ -1,44 +1,93 @@
 import skvideo.io as skio
 import cv2
+import random
 
 
 class SingleVideoEnv:
-    def __init__(self, filename):
-        __videodata = None
+    def __init__(self, filename, width, height):
+        self.__videodata = None
         try:
-            __videodata = skio.vread(filename)
+            self.__videodata = skio.vread(filename)
         except FileNotFoundError:
             print(filename + ' could not be opened')
             return False
-        __nb_frames, __h, __w, _ = __videodata.shape
+
+        with open(filename.split(".")[0] + ".labels", encoding='utf-8') as file:
+            l = file.readlines()
+        self.__labels = [x.strip() for x in l]
+
+
+        self.__nb_frames, _, _, _ = self.__videodata.shape
+        self.__w = width
+        self.__h = height
+        self.__index = 0
+        self.__correct = 0
 
     def reset(self):
-        # grab the next frame
-        # then retrieve and return
-        pass
+        # return the 0'th frame, advance the counter
+        self.__index = 0
+        img =  cv2.cvtColor(self.__videodata[self.__index, :], cv2.COLOR_RGB2BGR)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.resize(gray, (self.__w, self.__h), interpolation=cv2.INTER_AREA)
+        ngray = gray
+        cv2.normalize(gray, ngray, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+        self.__index += 1
+        return ngray
 
-    def next(self):
-        # see if there exists a next frame
-        # if yes, return it, o.w. return false
-        # this method can be private
-        pass
-    def reward(self):
-        # this method is private
-        # it covers all the possibilities of action
-        # must know labels and then it is possible to assign the reward
-        pass
     def step(self, taken_action):
         # return next frame,
         # return reward associated with prev frame for "taken_action"
         # return true if there is no next frame
-        pass
-    class action_space:
-        def __init__(self):
-            pass
-        def contains(self):
-            # return true for number within 0 to num_possible_states - 1
-            # o.w. false
-            pass
-        def sample(self):
-            #return a number at random in range 0 to num_possible_states - 1
-            pass
+        imgNext = self.__next()
+        eov_reached = False
+        if imgNext is None:
+            eov_reached = True
+        return imgNext, self.__reward(taken_action), eov_reached, self.__correct
+
+    def __next(self):
+        # see if there exists a next frame
+        # if yes, return it, o.w. return false
+        # this method can be private
+        self.__index += 1
+        if self.__index == self.__nb_frames:
+            return None
+        else:
+            img = cv2.cvtColor(self.__videodata[self.__index, :], cv2.COLOR_RGB2BGR)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            gray = cv2.resize(gray, (self.__w, self.__h), interpolation=cv2.INTER_AREA)
+            ngray = gray
+            cv2.normalize(gray, ngray, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+            return ngray
+
+    def __reward(self, a):
+        # this method is only suitable for this env
+        # it covers all the possibilities of action
+        # must know labels and then it is possible to assign the reward
+        if a == self.__labels[self.__index-1]:
+            self.__correct += 1
+            if a == 1:
+                return 10
+            else:
+                return 100
+        else:
+            if a == 1:
+                return -50
+            else:
+                return -100
+
+    # as of now, action space only has two actions, content = 1, non-content = 0
+class action_space:
+    def __init__(self):
+        self.__possible_actions = 2
+
+    def contains(self, action):
+        # return true for number within 0 to num_possible_actions - 1
+        # o.w. false
+        if action >= 0 and action < self.__possible_actions:
+            return True
+        else:
+            return False
+
+    def sample(self):
+        #return a number at random in range 0 to num_possible_states - 1
+        return random.randint(0, (self.__possible_actions - 1))
--- video.py
+++ video.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 from single_video_env import SingleVideoEnv
+from single_video_env import action_space
 
 def init_weights(shape):
     """ Weight initialization """
@@ -16,7 +17,8 @@ def forwardprop(X, w_1, w_2):
     yhat = tf.matmul(h, w_2)  # The \varphi function
     return yhat
 
-env = SingleVideoEnv('serve.mp4')
+env = SingleVideoEnv('serve.mp4', 32, 24)
+acs = action_space()
 
 
 x_size = 32*24
@@ -24,7 +26,7 @@ h_size = 256
 y_size = 2
 
 # Symbols
-X = tf.placeholder("float", shape=[None, x_size])
+X = tf.placeholder("float", shape=[1, x_size])
 y = tf.placeholder("float", shape=[None, y_size])
 
 # Weight initializations
@@ -45,14 +47,16 @@ init = tf.global_variables_initializer()
 # Set learning parameters
 y = .99
 e = 0.1
-num_episodes = 4000
+num_episodes = 100
 
 #create lists to contain total rewards
 rList = []
+correct = 0
 with tf.Session() as sess:
     sess.run(init)
     for i in range(num_episodes):
         # Reset environment = start video from the beginnning and get first new observation = first frame
+        print('iteration: ' + str(i))
         s = env.reset()
         rAll = 0
         d = False
@@ -60,29 +64,35 @@ with tf.Session() as sess:
             # Choose an action greedily (with e chance of random action) from the Q-network
             # feed_dict = {input_image_one_hot_encoded}
             # allQ has to be vector of 2, a is just max of that
-            a, allQ = sess.run([predict, Qout], feed_dict={X: np.identity(x_size)[s:s + 1]})
+            flatS = np.matrix(s.ravel())
+            a, allQ = sess.run([predict, Qout], feed_dict={X: np.array(flatS)})
             if np.random.rand(1) < e:
-                a[0] = env.action_space.sample()
+                a[0] = acs.sample()
             # Get new state and reward from environment
-            s1, r, d, _ = env.step(a[0])
+            s1, r, d, correct = env.step(a[0])
             # Obtain the Q' values by feeding the new state through our network
             # feed_dict={X: np.identity(x_size)[s1:s1 + 1]} is again, one hot encoded next image
-            Q1 = sess.run(Qout, feed_dict={X: np.identity(x_size)[s1:s1 + 1]})
-            # Obtain maxQ' and set our target value for chosen action.
-            maxQ1 = np.max(Q1)
-            targetQ = allQ
-            targetQ[0, a[0]] = r + y * maxQ1
-            # things are pretty much figured out till the line above
-            # *mind you, we are never ever passing the known (from oracle) labels to the neural network
-            # and ALL neural network learning is based on the rewardsd
-            # Train our network using target and predicted Q values
-            _ = sess.run([updateModel], feed_dict={X: np.identity(x_size)[s:s + 1], nextQ: targetQ})
-            rAll += r
-            s = s1
-            if d == True:
+            if (d == False):
+                flatS1 = np.matrix(s1.ravel())
+                Q1 = sess.run(Qout, feed_dict={X: np.array(flatS1)})
+                # Obtain maxQ' and set our target value for chosen action.
+                maxQ1 = np.max(Q1)
+                targetQ = allQ
+                targetQ[0, a[0]] = r + y * maxQ1
+                # things are pretty much figured out till the line above
+                # *mind you, we are never ever passing the known (from oracle) labels to the neural network
+                # and ALL neural network learning is based on the rewardsd
+                # Train our network using target and predicted Q values
+                _ = sess.run([updateModel], feed_dict={X: np.array(flatS), nextQ: targetQ})
+                rAll += r
+                s = s1
+            else:
                 # Reduce chance of random action as we train the model.
+                print('total correctly recognized: ' + str(correct))
                 e = 1. / ((i / 50) + 10)
         rList.append(rAll)
+
+
 print ("Percent of succesful episodes: " + str(sum(rList)/num_episodes) + "%")
 plt.plot(rList)
 plt.show()
